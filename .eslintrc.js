3629ffc857fdf05f2df74ef8ed146b07c786f958
Eduardo Vazquez <lalohao@gmail.com>
2018-07-25 02:09:22 -0500

    Jest support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: "airbnb",
   parser: "babel-eslint",
+  plugins: [
+    "jest",
+  ],
+  env: {
+    "jest/globals": true,
+  },
   rules: {
     "import/extensions": [2, "always"],
   },
